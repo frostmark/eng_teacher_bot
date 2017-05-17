401513d1e81b412c5f703f2130c72035445e706c
Artem Russkikh <rusartx@gmail.com>
2017-05-17 23:43:35 +0500

    Add dotenv

--- application/config/config.rb
+++ application/config/config.rb
@@ -1,6 +1,6 @@
 module Application
   module Config
-    CONFIG_FILE = YAML.load_file('config/config.yml')
+    CONFIG_FILE = YAML.load(ERB.new(File.read('config/config.yml')).result)
 
     BOT_TOKEN = CONFIG_FILE['bot_token']
     DB_NAME = CONFIG_FILE['database']['name']
