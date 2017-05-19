a5794cce5ec6016f3ca168ac182baf68ff28efd5
Mark Frost <cheerful.mf@gmail.com>
2017-05-19 23:53:50 +0500

    move out `add_item` into answers module
    and rename folder `answer` -> `answers`

---
+++ application/answers/add_item.rb
@@ -0,0 +1,34 @@
+module Application
+  module Answer
+    class AddItem
+      attr_reader :db, :message, :bot
+
+      def initialize(app)
+        @db = app.db
+        @message = app.message
+        @bot = app.bot
+      end
+
+      def call
+        words = message.text.match(/\s(.*\ -\ .*)/)[1].split(' - ')
+
+        doc = db.get(message.from.id.to_s)
+        unless doc['words'][words[0]]
+          doc['words'][words[0]] = {
+            'translate' => words[1],
+            'counter_repeat' => 0,
+            'date_at' => DateTime.now.to_s
+          }
+
+          if doc.save
+            bot.api.send_message(chat_id: message.chat.id, text: "#{words[0]} - #{words[1]} добавлено!")
+          else
+            bot.api.send_message(chat_id: message.chat.id, text: "Что-то пошло не так!")
+          end
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: "Такое слово уже есть!")
+        end
+      end
+    end
+  end
+end
