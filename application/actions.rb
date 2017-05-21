4fe6de026c7c55ecdae76adb875c8166f8ef2e2b
Mark Frost <cheerful.mf@gmail.com>
2017-05-22 00:51:02 +0500

    oops

--- application/actions.rb
+++ application/actions.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 module Application
-  class Actions::Base
-    attr_reader :message, :bot
+  module Actions
+    class Base
+      attr_reader :message, :bot
 
-    def initialize(app)
-      @message = app.message
-      @bot = app.bot
+      def initialize(app)
+        @message = app.message
+        @bot = app.bot
+      end
     end
   end
 end
--- application/models.rb
+++ application/models.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module Application
-  class Models::Base
-    def db
-      @db ||= Firebase::Client.new(Config::DB_URI)
+  module Models
+    class Base
+      def db
+        @db ||= Firebase::Client.new(Config::DB_URI)
+      end
     end
   end
 end
