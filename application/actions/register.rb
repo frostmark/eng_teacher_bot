54d79b11e22eda31b5fe958dfdaaa36cefa6af6b
Mark Frost <cheerful.mf@gmail.com>
2017-05-22 00:08:08 +0500

    user model and action register

--- application/actions/register.rb
+++ application/actions/register.rb
@@ -2,7 +2,28 @@ module Application
   module Actions
     class Register < Base
       def call
-        Models::User.new.create
+        user_model = Models::User.new
+
+        user = user_model.find(message.from.id)
+        if user
+          bot.api.send_message(chat_id: message.chat.id, text: 'Ты зарегистрирован уже!')
+        else
+          user_model.create user_params
+        end
+      end
+
+      private
+
+      def user_params
+        {
+          "#{message.from.id}" => {
+            name: "#{message.from.first_name} #{message.from.last_name}",
+            telegram: {
+              username: "#{message.from.username}"
+            },
+            items_count: 0
+          }
+        }
       end
     end
   end
--- application/models/user.rb
+++ application/models/user.rb
@@ -1,8 +1,16 @@
 module Application
   module Models
     class User < Base
-      def create
+      def create(data)
+        db.set('/users', data)
+      end
 
+      def find(id)
+        db.get("/users/#{id}")
+      end
+
+      def update(id, data)
+        db.update("/users/#{id}", data)
       end
     end
   end
