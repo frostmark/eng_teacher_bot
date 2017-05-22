c7de57f8b58a1ea7b9ed1fa2073e25e5c4426495
Artem Russkikh <rusartx@gmail.com>
2017-05-23 02:35:15 +0500

    Add example job

---
+++ application/jobs/reminder.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Application
+  class Jobs::Reminder < Jobs::Base
+    include Sidekiq::Worker
+
+    def perform(how_hard="super hard", how_long=1)
+      logger.info 'perfroming...'
+      sleep how_long
+      logger.info "Workin' #{how_hard}"
+    end
+  end
+end
